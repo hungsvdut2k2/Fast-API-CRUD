ad72c4a0180673f504186d364ac92ea92508f37c
hungsvdut2k2 <hungsvdut@gmail.com>
2023-01-02 12:13:39 +0700

    feat: add crud of post

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from fastapi import Body
 from pydantic import BaseModel
 from typing import Optional
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
@@ -9,7 +11,15 @@ app = FastAPI()
 class Post(BaseModel):
     title: str
     content: str
-    rating: Optional[int] = None
+    published: bool
+
+
+try:
+    conn = psycopg2.connect(
+        host='localhost', database='Fast_API', user='postgres', password='Hung18062002', cursor_factory=RealDictCursor)
+    cursor = conn.cursor()
+except:
+    print("Error")
 
 
 @app.get("/")
@@ -17,11 +27,49 @@ def root():
     return {"message": "Hello World"}
 
 
+@app.get("/posts")
+def get_posts():
+    sql = 'SELECT * FROM posts'
+    cursor.execute(sql)
+    posts = cursor.fetchall()
+    return {"posts": posts}
+
+
 @app.get("/posts/{id}")
-def get_one_post(id):
-    return id
+def get_one_post(id: int):
+    sql = f"SELECT * FROM posts WHERE id = {str(id)}"
+    cursor.execute(sql)
+    post = cursor.fetchone()
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return {"post": post}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    return {"payload": post}
+    sql = f'INSERT INTO posts (title, content, published) VALUES {post.title, post.content, post.published}'
+    cursor.execute(sql)
+    conn.commit()
+    return {}
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
+                   (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    if not updated_post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    conn.commit()
+    return
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    sql = f"DELETE FROM posts WHERE id = {str(id)} RETURNING *"
+    cursor.execute(sql)
+    deleted_post = cursor.fetchone()
+    if not deleted_post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    conn.commit()
+    return {}
