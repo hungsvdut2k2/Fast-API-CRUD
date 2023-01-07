ef73baf6a3f5e80198a715a602babcd00c01ef14
hungsvdut2k2 <hungsvdut@gmail.com>
2023-01-07 15:48:19 +0700

    feat: add response model

--- app/main.py
+++ app/main.py
@@ -1,17 +1,12 @@
 from fastapi import FastAPI, status, HTTPException, Depends
-from pydantic import BaseModel
 from sqlalchemy.orm import Session
-from . import models
+from . import models, schemas
 from .database import engine, get_db
+from typing import List
+
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
-
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool
 
 
 @app.get("/")
@@ -19,36 +14,36 @@ def root():
     return {"message": "Hello World"}
 
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
-    return {"posts": posts}
+    return posts
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def get_one_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=404, detail="Post not found")
-    return {"post": post}
+    return post
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
-    return {}
+    return
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post, db: Session = Depends(get_db)):
+def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
     updated_post = db.query(models.Post).filter(models.Post.id == id)
     if not updated_post.first():
         raise HTTPException(status_code=404, detail="Post not found")
     updated_post.update(post.dict(), synchronize_session=False)
     db.commit()
-    return {}
+    return
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -58,9 +53,4 @@ def delete_post(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Post not found")
     post.delete(synchronize_session=False)
     db.commit()
-    return {}
-
-
-# comment
-# other comment
-# the other comment
+    return
