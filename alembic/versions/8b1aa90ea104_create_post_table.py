24e5c30c1fa7d0171ef72e053b5cb5034140e2d6
hungsvdut2k2 <hungsvdut@gmail.com>
2023-01-09 17:09:17 +0700

    feat: add alembic for database migration

---
+++ alembic/versions/8b1aa90ea104_create_post_table.py
@@ -0,0 +1,24 @@
+"""create post table
+
+Revision ID: 8b1aa90ea104
+Revises: 
+Create Date: 2023-01-09 17:07:29.478350
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8b1aa90ea104'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
